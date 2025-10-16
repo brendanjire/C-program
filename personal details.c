9a21ccaa33f8fd2cd1feafe9bf22f3ca1affa56e
brendanjire <brianjire015@gmail.com>
2025-10-16 10:34:20 +0300

    Add files via upload

---
+++ personal details.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+/*
+Brenda Njire
+Kirinyaga university
+Computer science
+*/
+float height;
+double bank_balance;
+int phone_number;
+printf("Enter your height:\n");
+scanf("%f",&height);
+printf("Enter your bank balance:\n");
+scanf("%lf",&bank_balance);
+printf("Enter your phone number:\n");
+scanf("%d",&phone_number);
+
+
+
+    return 0;
+}

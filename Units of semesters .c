ebc658b3bbd893fa4d7fc72bc2b64d917a3d01d8
brendanjire <brianjire015@gmail.com>
2025-10-16 10:20:45 +0300

    Add files via upload

---
+++ Units of semesters .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+printf("introduction to programming and problem solving\n");
+printf("computer architecture\n");
+printf("analogue electronics\n");
+printf("discrete mathematics\n");
+printf("communication skills\n");
+printf("fundamentals of ict\n");
+printf("mathematics for science\n");
+printf("physics\n");
+
+
+
+    return 0;
+}

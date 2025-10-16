6c1206418f9dc1f7d08879b1f9f48a649394156d
brendanjire <brianjire015@gmail.com>
2025-10-16 10:37:33 +0300

    Add files via upload

---
+++ variables (1).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+char a='k'; //%c
+char name[]=("Agnes"); //%s
+int age=10; //%d
+float pi=3.142; //%f
+double profit=10000.10; //%lf
+
+printf("The value is %c\n",a);
+printf("The name is %s\n",name);
+printf("The age is %d\n",age);
+printf("The pi is %.2f\n",pi);
+printf("The profit is %.5lf\n",profit);
+
+
+    return 0;
+}

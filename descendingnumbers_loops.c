2df1c8623535efeb4c60399300b5eff6bb4efe7d
brendanjire <brianjire015@gmail.com>
2025-11-06 22:59:08 +0300

    Add files via upload

---
+++ descendingnumbers_loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+Brenda Njire
+CT101/G/26465/25
+Kirinyaga university
+Computer science
+For loop
+*/
+int main() {
+    for (int i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
